061073b541aa9f4a83bfa7850fefdcd87cd1f56e
alinne <alinnegraziellecontato@gmail.com>
2021-03-28 18:28:46 -0300

    created HomePage

--- app_financial_transactions/lib/main.dart
+++ app_financial_transactions/lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
+import 'dart:math';
+
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Banco Exemplo S/A',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
-  }
 }
 
 class HomePage extends StatelessWidget {
