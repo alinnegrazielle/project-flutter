278ef46d207928fd60190e7d1b2368ae07bce6d9
alinne <alinnegraziellecontato@gmail.com>
2021-03-29 21:27:11 -0300

    Revert "created a file for transaction logic"
    This reverts commit 09c8823e16406f5a261b2748e6b7df3757d6e847.

--- app_financial_transactions/lib/main.dart
+++ app_financial_transactions/lib/main.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
 
-import 'home_page_screen.dart';
+import 'home_page.dart';
+
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Banco Exemplo S/A',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Banco Exemplo S/A"),
       ),
-      home: HomePage(),
+      body: Container(
+        child: Text("EXTRATO"),
+      ),
     );
   }
 }
+
+// class GeneratedActions extends StatefulWidget {
+//   @override
+//   _GeneratedActionsState createState() {
+//     return _GeneratedActionsState();
+//   }
+// }
+
+// class _GeneratedActionsState extends State<GeneratedActions> {
+//   random(min, max){
+//     var rn = new Random();
+//     return min + rn.nextInt(max - min);
+//   }
+// }
+
+// print(random(5, 20));
